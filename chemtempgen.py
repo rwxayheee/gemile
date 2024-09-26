cfb3d93edae09023ceb759ebbd60a955e8874676
Amy He <ayhe1768@gmail.com>
2024-09-26 12:21:56 -0700

    reviewed and beautified script

--- chemtempgen.py
+++ chemtempgen.py
@@ -1,130 +1,104 @@
 from rdkit import Chem
 import gemmi
 
-def get_smiles_with_atom_names(mol):
-    # Generate SMILES with explicit hydrogens
+def get_smiles_with_atom_names(mol: Chem.Mol) -> tuple[str, list[str]]:
+    """Generate SMILES with atom names in the order of SMILES output."""
+    # allHsExplicit may expose the implicit Hs of linker atoms to Smiles; the implicit Hs don't have names
     smiles_exh = Chem.MolToSmiles(mol, allHsExplicit=True)
 
-    # Get SMILES output order from molecule properties
     smiles_atom_output_order = mol.GetProp('_smilesAtomOutputOrder')
-        
-    # Clean the order string to get a list of indices
     delimiters = ['[', ']', ',']
     for delimiter in delimiters:
         smiles_atom_output_order = smiles_atom_output_order.replace(delimiter, ' ')
     smiles_output_order = [int(x) for x in smiles_atom_output_order.split()]
 
-    # Re-index atom names based on the SMILES output order
-    atom_name = []
-    for atom_i in smiles_output_order:
-        atom_name.append(mol.GetAtomWithIdx(atom_i).GetProp('atom_id'))  # Get atom name using 'atom_id' property
+    atom_name = [mol.GetAtomWithIdx(atom_i).GetProp('atom_id') for atom_i in smiles_output_order]
 
     return smiles_exh, atom_name
 
 
-def embed(mol, leaving_name: list[str]):
+def embed(mol: Chem.Mol, leaving_name: list[str]):
+    """Remove atoms from the molecule based on the leaving_name set."""
+    leaving_atoms = [atom for atom in mol.GetAtoms() if atom.GetProp('atom_id') in leaving_name] 
 
-    init_atoms = mol.GetAtoms()
-    leaving_atoms = [atom for atom in init_atoms if atom.GetProp('atom_id') in leaving_name]
-
-    if len(leaving_atoms) == 0: 
-        print(f"no leaving atoms for {leaving_name}...")
+    if not leaving_atoms: 
+        print(f"No leaving atoms in {leaving_name} found in mol...")
         return mol
-    else:
-        sorted_leaving_atoms = sorted(leaving_atoms, key=lambda atom: atom.GetIdx(), reverse=True)
-
-    rwmol = Chem.RWMol(mol)
-    for atom in sorted_leaving_atoms:
-        rwmol.RemoveAtom(atom.GetIdx())
     
+    rwmol = Chem.RWMol(mol)
+    # remove from the largest index to preserve smaller indexes
+    for atom in sorted(leaving_atoms, key=lambda atom: atom.GetIdx(), reverse=True): 
+        rwmol.RemoveAtom(atom.GetIdx()) # linker atoms will have implicit Hs to compensate missing connections
+
     return rwmol.GetMol()
 
 
-def deprotonate(mol, acidic_proton_loc = {'[H][O][PX4](=O)([O])[OX2]': 0}):
+def deprotonate(mol, acidic_proton_loc: dict[str, int]) -> Chem.Mol:
+    """Remove acidic protons from the molecule based on acidic_proton_loc"""
+    # acidic_proton_loc is a mapping 
+    # keys: smarts pattern of a fragment
+    # value: the index (order in smarts) of the leaving proton
 
+    # deprotonate all matched protons
     acidic_protons = []
-    for smarts_pattern in acidic_proton_loc:
+    for smarts_pattern, idx in acidic_proton_loc.items():
         qmol = Chem.MolFromSmarts(smarts_pattern)
-        matches = mol.GetSubstructMatches(qmol)
-        for match in matches:
-            acidic_protons.append(mol.GetAtomWithIdx(match[acidic_proton_loc[smarts_pattern]]))
+        acidic_protons.extend([mol.GetAtomWithIdx(match[idx]) for match in mol.GetSubstructMatches(qmol)])
     
-    if len(acidic_protons) == 0:
-        print(f"no acidic protons for {acidic_proton_loc.keys()}")
+    if not acidic_protons:
+        print(f"No acidic protons in {acidic_proton_loc.keys()} found in mol")
         return mol
-    else:
-        sorted_acidic_protons = sorted(acidic_protons, key=lambda atom: atom.GetIdx(), reverse=True)
     
     rwmol = Chem.RWMol(mol)
-    for atom in sorted_acidic_protons:
+    for atom in sorted(acidic_protons, key=lambda atom: atom.GetIdx(), reverse=True):
         rwmol.RemoveAtom(atom.GetIdx())
         neighbors = atom.GetNeighbors()
         if neighbors:
-            neighbor_idx = neighbors[0].GetIdx()
-            neighbor_atom = rwmol.GetAtomWithIdx(neighbor_idx)
-            neighbor_atom_charge = neighbor_atom.GetFormalCharge()
-            neighbor_atom.SetFormalCharge(neighbor_atom_charge-1)
+            neighbor_atom = rwmol.GetAtomWithIdx(neighbors[0].GetIdx())
+            neighbor_atom.SetFormalCharge(neighbor_atom.GetFormalCharge() - 1)
     
     return rwmol.GetMol()
 
 
-def is_chemical_element(symbol):
-    # Get the periodic table from RDKit
-    periodic_table = Chem.GetPeriodicTable()
-    
-    # Try to get the atomic number of the element using the symbol
+def is_chemical_element(symbol: str) -> bool:
+    """Check if a string represents a valid chemical element."""
     try:
-        atomic_number = periodic_table.GetAtomicNumber(symbol)
-        # If atomic_number is greater than 0, it's a valid element
-        return atomic_number > 0
-    except:
-        # If symbol is not a valid element, GetAtomicNumber raises a ValueError
+        return Chem.GetPeriodicTable().GetAtomicNumber(symbol) > 0
+    # rdkit throws RuntimeError if invalid
+    except RuntimeError:
         return False
 
 
 def make_pretty_smiles(smi: str): 
-    # remove square brackets if atom does not have a charge;
-    # make H implicit
-
+    """Convert Smiles with allHsExplicit to pretty Smiles to be put on chem templates"""
+    # collect the inside square brackets contents
     contents = set()
-
+    inside_bracket = False
     for char in smi:
         if char == '[':
             inside_bracket = True
-            content = ""  # Start a new element
+            content = ""
         elif char == ']' and inside_bracket:
             inside_bracket = False
-            contents.add(content)  # Add the completed element
+            contents.add(content)
         elif inside_bracket:
-            content += char  # Collect characters inside brackets
+            content += char
 
     for content in contents:
-        if content=='H':
-            pass
-        elif is_chemical_element(content):
-            smi = smi.replace(f"[{content}]", f"{content}")
-        elif 'H' in content:
-            H_stripped = content.split('H')[0]
-            if is_chemical_element(H_stripped):
-                smi = smi.replace(f"[{content}]", f"{H_stripped}")
-
+        # keep [H] for explicit Hs
+        if content == 'H': 
+            continue
+        # drop H in the content to hide implicit Hs
+        H_stripped = content.split('H')[0]
+        # drop [ ] if the content is an uncharged element symbol
+        if is_chemical_element(content) or is_chemical_element(H_stripped):
+            smi = smi.replace(f"[{content}]", f"{H_stripped}" if 'H' in content else f"{content}")
     return smi
-
-
-def make_link_labels_from_names(names: list[str], name_to_label_mapping = {'P': '5-prime', "O3'": '3-prime'}):
-    # find link labels by names
-    link_labels = {}
-    for name in name_to_label_mapping: 
-        if name in names:
-            link_labels[str(names.index(name))] = name_to_label_mapping[name]
-
-    return link_labels
 
 
 class ChemicalComponent:
 
-    def __init__(self, rdkit_mol: Chem.Mol, \
-                 resname: str, smiles_exh: str, atom_name: list[str], leaving_name: set[str]):
+    def __init__(self, rdkit_mol: Chem.Mol, resname: str, smiles_exh: str, atom_name: list[str], leaving_name: set[str]):
         self.rdkit_mol = rdkit_mol
         self.resname = resname
         self.smiles_exh = smiles_exh
@@ -133,11 +107,12 @@ class ChemicalComponent:
         self.link_labels = {} # default to empty dict (free molecular form)
         self.parent = resname # default parent to itself
     
-    @classmethod # requires gemmi
-    def from_cif(cls, source_cif: str, CCD_resname: str):
-        resname = CCD_resname # default resname to CCD_resname
-
-        # Locate block
+    @classmethod
+    # requires gemmi
+    def from_cif(cls, source_cif: str, resname: str):
+        """Create ChemicalComponent from a chemical component dict file and a searchable residue name in file."""
+        
+        # Locate block by resname
         doc = gemmi.cif.read(source_cif)
         block = doc.find_block(resname)
         
@@ -157,16 +132,14 @@ class ChemicalComponent:
 
         for idx, element in enumerate(atom_elements):
             rdkit_atom = Chem.Atom(element)
-            
             for attr in atom_attributes:
                 rdkit_atom.SetProp(attr, atom_cols[attr][idx])
+                # strip double quotes in names
                 raw_name = rdkit_atom.GetProp('atom_id')
                 rdkit_atom.SetProp('atom_id', raw_name.strip('"'))
-            
-            target_charge = rdkit_atom.GetProp('charge')
+            target_charge = atom_cols['charge'][idx]
             if target_charge!='0':
-                rdkit_atom.SetFormalCharge(int(target_charge))
-            
+                rdkit_atom.SetFormalCharge(float(target_charge)) # this is usually int
             rwmol.AddAtom(rdkit_atom)
 
         # Map atom_id (atom names) with rdkit idx
@@ -191,45 +164,52 @@ class ChemicalComponent:
         bond_types = bond_cols['value_order']
 
         for bond_i, bond_type in enumerate(bond_types):
-            if bond_type not in bond_type_mapping:
-                raise ValueError(f"value_order {bond_type} from input is not in {bond_type_mapping.keys()}")
-            else:
-                rwmol.AddBond(name_to_idx_mapping[bond_cols['atom_id_1'][bond_i].strip('"')], \
-                            name_to_idx_mapping[bond_cols['atom_id_2'][bond_i].strip('"')], \
-                            bond_type_mapping[bond_type])
+            rwmol.AddBond(name_to_idx_mapping[bond_cols['atom_id_1'][bond_i].strip('"')], 
+                          name_to_idx_mapping[bond_cols['atom_id_2'][bond_i].strip('"')], 
+                          bond_type_mapping.get(bond_type, Chem.BondType.UNSPECIFIED))
 
-        smiles_exh, atom_name = get_smiles_with_atom_names(rwmol)
+        # Finish eidting mol    
         for atom in rwmol.GetAtoms():
             atom.UpdatePropertyCache()
         rdkit_mol = rwmol.GetMol()
-
-        leaving_name = {atom_cols['atom_id'][atom_i].strip('"')
-                        for atom_i, atom_leaving_flag in enumerate(atom_cols['pdbx_leaving_atom_flag']) 
-                        if atom_leaving_flag == 'Y'}
-        print(f"leaving atoms from pdbx annotations: {leaving_name}")
             
+        # Get Smiles with explicit Hs and ordered atom names
+        smiles_exh, atom_name = get_smiles_with_atom_names(rdkit_mol)
 
+        # Get set of leaving atom names
+        leaving_name = {atom_cols['atom_id'][atom_i].strip('"')
+                        for atom_i, flag in enumerate(atom_cols['pdbx_leaving_atom_flag']) 
+                        if flag == 'Y'}
+        if leaving_name:
+            print(f"leaving atoms from pdbx annotations: {leaving_name}")
+        
         return cls(rdkit_mol, resname, smiles_exh, atom_name, leaving_name)
 
 
-    def make_embedded(self): 
-        newmol = embed(self.rdkit_mol, self.leaving_name)
-        print(f"Current leaving atoms: {self.leaving_name}")
-        
-        self.smiles_exh, self.atom_name = get_smiles_with_atom_names(newmol)
-        self.rdkit_mol = newmol
-    
+    def make_canonical(self):
+        """Deprotonate acidic groups til the canonical (most deprotonated) state."""
+        self.rdkit_mol = deprotonate(self.rdkit_mol, acidic_proton_loc = {'[H][O][PX4](=O)([O])[OX2]': 0})
+        self.smiles_exh, self.atom_name = get_smiles_with_atom_names(self.rdkit_mol)
 
-    def make_canonical(self): 
-        newmol = deprotonate(self.rdkit_mol)
+    def make_embedded(self):
+        """Remove leaving atoms from the molecule."""
+        self.rdkit_mol = embed(self.rdkit_mol, self.leaving_name)
+        self.smiles_exh, self.atom_name = get_smiles_with_atom_names(self.rdkit_mol)
 
-        self.smiles_exh, self.atom_name = get_smiles_with_atom_names(newmol)
-        self.rdkit_mol = newmol
+    def make_link_labels_from_names(self, name_to_label_mapping = {'P': '5-prime', "O3'": '3-prime'}):
+        """Map atom names to link labels based on a given mapping."""
+        link_labbels = {}
+        for atom in self.rdkit_mol.GetAtoms():
+            if atom.GetProp('atom_id') in name_to_label_mapping:
+                if atom.GetNumImplicitHs() > 0:
+                    name = atom.GetProp('atom_id')
+                    link_labbels[str(self.atom_name.index(name))] = name_to_label_mapping[name]
+        self.link_labels = link_labbels
 
 
-# XXX make embedding variants, assign/check link_labels and update smiles/idx
-# XXX make? read? protonation variants, alter charge and update smiles/idx
-# XXX group by parent: variants for "ambiguous"
+# XXX make embedding variants by PATTERNS not names, assign/check link_labels and update smiles/idx
+# XXX read from prepared? enumerate in stepwise? all protonation state variants, alter charge and update smiles/idx
+# XXX group by parent: variants for "ambiguous"; tag synonym Amber and CCD resnames
 
 
 if __name__ == '__main__':
@@ -254,7 +234,7 @@ if __name__ == '__main__':
             cc.make_embedded()
 
             cc.smiles_exh = make_pretty_smiles(cc.smiles_exh)
-            cc.link_labels = make_link_labels_from_names(cc.atom_name)
+            cc.link_labels = cc.make_link_labels_from_names()
             cc.resname += suffix
             cc.parent = basename
 
@@ -286,7 +266,7 @@ if __name__ == '__main__':
             cc.make_embedded()
 
             cc.smiles_exh = make_pretty_smiles(cc.smiles_exh)
-            cc.link_labels = make_link_labels_from_names(cc.atom_name)
+            cc.link_labels = cc.make_link_labels_from_names(cc.atom_name)
             cc.resname += suffix
             cc.parent = basename
 

2b3c702c24cf76faae12ea26bcee27db6d475380
Amy He <ayhe1768@gmail.com>
2024-11-28 13:55:00 -0800

    (wip) parse bld

--- parse_bld.py
+++ parse_bld.py
@@ -2,74 +2,63 @@ import re
 from collections import namedtuple
 
 def parse_bld_file(bld_filename): 
-    with open(bld_filename, 'r') as f:
-        bld_lines = f.readlines()
     bld_blocks = []
-    bld_block = []
-    for line in bld_lines:
-        if line.startswith("f_m_ct"):
-            if len(bld_block) > 0: 
-                bld_blocks.append(bld_block)
-            bld_block = [line]
-        else:
-            bld_block.append(line)
-    if len(bld_block) > 0: 
-        bld_blocks.append(bld_block)
+    with open(bld_filename, 'r') as f:
+        bld_lines = (line.strip() for line in f) 
+        current_block = []
+        for line in bld_lines:
+            if line.startswith("f_m_ct"):
+                if current_block: 
+                    bld_blocks.append(current_block)
+                current_block = [line] 
+            else:
+                current_block.append(line)
+        if current_block:
+            bld_blocks.append(current_block)
     return bld_blocks
 
-def parse_bld_block(bld_block): 
-    # assumes first subsection is title
+def extract_block(start_marker, end_marker, bld_block, start_index):
+    """Extracts lines between a start marker and end marker."""
+    block = []
+    line_number = start_index
+    max_line_number = len(bld_block) - 1
+    # Find the start marker
+    while start_marker not in bld_block[line_number] and line_number < max_line_number:
+        line_number += 1
+    # Skip the line with the start marker
+    line_number += 1
+    # Extract lines until the end marker
+    while line_number < max_line_number and end_marker not in bld_block[line_number]:
+        block.append(bld_block[line_number])
+        line_number += 1
+    return block, line_number
+
+def extract_title(bld_block):
     title_block = []
-    line_number = 1
-    max_line_number = len(bld_block) - 2
-    line = bld_block[line_number]
-    while "{" not in line and line_number<max_line_number: 
-        title_block.append(line.strip())
+    line_number = 1  # Start after the first line
+    max_line_number = len(bld_block) - 1
+    while line_number < max_line_number and "{" not in bld_block[line_number]:
+        title_block.append(bld_block[line_number])
         line_number += 1
-        line = bld_block[line_number]
-    title_dict = parse_mapping(title_block)
+    return title_block, line_number
 
-    # assumes atom section starts with m_atom[<number of atoms>]
-    # followed by bond section that starts with m_bond[<number of bonds>]
-    atom_block = []
-    bond_block = []
-    while "m_atom" not in line and line_number<max_line_number:
-        line_number += 1
-        line = bld_block[line_number]
-    line_number += 1
-    line = bld_block[line_number]
-    while "}" not in line and line_number<max_line_number:
-        atom_block.append(line.strip())
-        line_number += 1
-        line = bld_block[line_number]
-    while "m_bond" not in line and line_number<max_line_number:
-        line_number += 1
-        line = bld_block[line_number]
-    line_number += 1
-    line = bld_block[line_number]
-    while "}" not in line and line_number<max_line_number:
-        bond_block.append(line.strip())
-        line_number += 1
-        line = bld_block[line_number]
+def parse_bld_block(bld_block):
+    title_block, line_number = extract_title(bld_block)
+    atom_block, line_number = extract_block("m_atom", "}", bld_block, line_number)
+    bond_block, _ = extract_block("m_bond", "}", bld_block, line_number)
 
-    if atom_block: 
-        atom_list = parse_table(atom_block)
-    else:
-        return {"title": title_dict, "atom_list": [], "bond_list": []}
-
-    if bond_block: 
-        bond_list = parse_table(bond_block)
-    else:
-        return {"title": title_dict, "atom_list": atom_list, "bond_list": []}
-
-    return {"title": title_dict, "atom_list": atom_list, "bond_list": bond_list}
+    return {
+        "title_block": parse_mapping(title_block),
+        "atom_block": parse_table(atom_block),
+        "bond_block": parse_table(bond_block),
+    }
 
 def parse_mapping(input_block): 
     parsed_dict = {}
     keys = []
     is_val_row = False
     for line in input_block: 
-        line = line.strip()
+        line = line
         if ":::" in line:
             is_val_row = True
         elif not is_val_row: 
@@ -85,7 +74,7 @@ def parse_table(input_block):
     is_obj_row = False
     bld_obj = None
     for line in input_block:
-        line = line.strip()
+        line = line
         if ":::" in line: 
             bld_obj = namedtuple('bld_obj', obj_fields)
             is_obj_row = True
@@ -108,8 +97,7 @@ def parse_line_to_tuple(line):
     matches = re.findall(r'("[^"]*"|\S+)', line)
     return tuple(match.strip('"') if match.startswith('"') and match.endswith('"') else match for match in matches)
 
-bld_blocks = parse_bld_file("/Users/amyhe/Desktop/0_forks/template_mod/res/dna2.bld")
+bld_blocks = parse_bld_file("/Users/amyhe/Desktop/0_forks/template_mod/res/dna.bld")
 bld_block_1 = bld_blocks[1]
 parse_bld_block(bld_block_1)
-
 
